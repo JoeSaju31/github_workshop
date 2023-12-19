223eec927c68959b40c919057d3688da73b8e926
joeljohn31 <mjpjoel@gmail.com>
2023-12-19 11:54:53 +0530

    Updated the program

---
+++ fact.py
@@ -0,0 +1,11 @@
+import os
+
+def factorial(n):
+    if n==0:
+        return 1
+    return n*factorial(n-1)
+
+print("Enter a number:")
+n=int(input())
+f=factorial(n)
+print(f"Factorial of {n} is {f}")
